29a7ef0f2a92721285ba5fd7654a63425f76f175
Luke Edwards <luke.edwards05@gmail.com>
2021-03-02 19:12:04 -0800

    chore: update test runner

--- test/index.js
+++ test/index.js
@@ -3,146 +3,149 @@
  * @see https://github.com/lukeed/zeit-ms/blob/master/tests.js
  */
 
-import test from 'tape';
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
 import * as ms from '../src';
 
-test.Test.prototype.parse = function (input, expect) {
+function parse(input, expect) {
 	const output = ms.parse(input);
-	this.is(output, expect, `parse("${input}") ~> ${expect}`);
-	this.is(typeof output, typeof expect, `~> returns "${typeof expect}" type`);
+	assert.is(output, expect, `parse("${input}") ~> ${expect}`);
+	assert.is(typeof output, typeof expect, `~> returns "${typeof expect}" type`);
 }
 
-test.Test.prototype.format = function (input, expect, long) {
+function format(input, expect, long) {
 	const output = ms.format(input, long);
-	this.is(output, expect, `format(${input}) ~> "${expect}"`);
-	this.is(typeof output, typeof expect, `~> returns "${typeof expect}" type`);
+	assert.is(output, expect, `format(${input}) ~> "${expect}"`);
+	assert.is(typeof output, typeof expect, `~> returns "${typeof expect}" type`);
 }
 
-test('exports', t => {
-	t.is(typeof ms, 'object', 'exports an object');
-	t.is(typeof ms.parse, 'function', '~> has "parse" function');
-	t.is(typeof ms.format, 'function', '~> has "format" function');
-	t.end();
+// ---
+
+test('exports an object', () => {
+	assert.type(ms, 'object');
+});
+
+test('exports "parse" function', () => {
+	assert.type(ms.parse, 'function');
+});
+
+test('exports "format" function', () => {
+	assert.type(ms.format, 'function');
 });
 
 
-test('parse()', t => {
-	t.parse('100', 100);
-	t.parse('1m', 60000);
-	t.parse('1h', 3600000);
-	t.parse('2d', 172800000);
-	t.parse('3w', 1814400000);
-	t.parse('1s', 1000);
-	t.parse('100ms', 100);
-	t.parse('1.5h', 5400000);
-	t.parse('1   s', 1000);
+test('parse()', () => {
+	parse('100', 100);
+	parse('1m', 60000);
+	parse('1h', 3600000);
+	parse('2d', 172800000);
+	parse('3w', 1814400000);
+	parse('1s', 1000);
+	parse('100ms', 100);
+	parse('1.5h', 5400000);
+	parse('1   s', 1000);
 
-	t.parse('☃', undefined);
-	t.parse('10-.5', undefined);
+	parse('☃', undefined);
+	parse('10-.5', undefined);
 
-	t.parse('1.5H', 5400000);
-	t.parse('.5ms', 0.5);
-	t.parse('-100ms', -100);
-	t.parse('-1.5h', -5400000);
-	t.parse('-10.5h', -37800000);
-	t.parse('-.5h', -1800000);
-
-	t.end();
+	parse('1.5H', 5400000);
+	parse('.5ms', 0.5);
+	parse('-100ms', -100);
+	parse('-1.5h', -5400000);
+	parse('-10.5h', -37800000);
+	parse('-.5h', -1800000);
 });
 
 
-test('parse() :: long', t => {
-	t.parse('53 milliseconds', 53);
-	t.parse('17 msecs', 17);
-	t.parse('1 sec', 1000);
-	t.parse('1 min', 60000);
-	t.parse('1 hr', 3600000);
-	t.parse('2 days', 172800000);
-	t.parse('1.5 hours', 5400000);
-	t.parse('-100 milliseconds', -100);
-	t.parse('-1.5 hours', -5400000);
-	t.parse('-.5 hr', -1800000);
-
-	t.end();
+test('parse() :: long', () => {
+	parse('53 milliseconds', 53);
+	parse('17 msecs', 17);
+	parse('1 sec', 1000);
+	parse('1 min', 60000);
+	parse('1 hr', 3600000);
+	parse('2 days', 172800000);
+	parse('1.5 hours', 5400000);
+	parse('-100 milliseconds', -100);
+	parse('-1.5 hours', -5400000);
+	parse('-.5 hr', -1800000);
 });
 
 
-test('format()', t => {
-	t.format(500, '500ms');
-	t.format(-500, '-500ms');
+test('format()', () => {
+	format(500, '500ms');
+	format(-500, '-500ms');
 
 	// seconds
-	t.format(1000, '1s');
-	t.format(10000, '10s');
-	t.format(-1000, '-1s');
-	t.format(-10000, '-10s');
+	format(1000, '1s');
+	format(10000, '10s');
+	format(-1000, '-1s');
+	format(-10000, '-10s');
 
 	// minutes
-	t.format(60 * 1000, '1m');
-	t.format(60 * 10000, '10m');
-	t.format(-1 * 60 * 1000, '-1m');
-	t.format(-1 * 60 * 10000, '-10m');
+	format(60 * 1000, '1m');
+	format(60 * 10000, '10m');
+	format(-1 * 60 * 1000, '-1m');
+	format(-1 * 60 * 10000, '-10m');
 
 	// hours
-	t.format(60 * 60 * 1000, '1h');
-	t.format(60 * 60 * 10000, '10h');
-	t.format(-1 * 60 * 60 * 1000, '-1h');
-	t.format(-1 * 60 * 60 * 10000, '-10h');
+	format(60 * 60 * 1000, '1h');
+	format(60 * 60 * 10000, '10h');
+	format(-1 * 60 * 60 * 1000, '-1h');
+	format(-1 * 60 * 60 * 10000, '-10h');
 
 	// days
-	t.format(24 * 60 * 60 * 1000, '1d');
-	t.format(24 * 60 * 60 * 10000, '10d');
-	t.format(-1 * 24 * 60 * 60 * 1000, '-1d');
-	t.format(-1 * 24 * 60 * 60 * 10000, '-10d');
+	format(24 * 60 * 60 * 1000, '1d');
+	format(24 * 60 * 60 * 10000, '10d');
+	format(-1 * 24 * 60 * 60 * 1000, '-1d');
+	format(-1 * 24 * 60 * 60 * 10000, '-10d');
 
 	// rounding
-	t.format(234234234, '3d');
-	t.format(-234234234, '-3d');
-
-	t.end();
+	format(234234234, '3d');
+	format(-234234234, '-3d');
 });
 
 
-test('format() :: long', t => {
+test('format() :: long', () => {
 	// milliseconds
-	t.format(500, '500 ms', true);
-	t.format(-500, '-500 ms', true);
+	format(500, '500 ms', true);
+	format(-500, '-500 ms', true);
 
 	// seconds
-	t.format(1000, '1 second', true);
-	t.format(1200, '1 second', true);
-	t.format(10000, '10 seconds', true);
-	t.format(-1000, '-1 second', true);
-	t.format(-1200, '-1 second', true);
-	t.format(-10000, '-10 seconds', true);
+	format(1000, '1 second', true);
+	format(1200, '1 second', true);
+	format(10000, '10 seconds', true);
+	format(-1000, '-1 second', true);
+	format(-1200, '-1 second', true);
+	format(-10000, '-10 seconds', true);
 
 	// minutes
-	t.format(60 * 1000, '1 minute', true);
-	t.format(60 * 1200, '1 minute', true);
-	t.format(60 * 10000, '10 minutes', true);
-	t.format(-1 * 60 * 1000, '-1 minute', true);
-	t.format(-1 * 60 * 1200, '-1 minute', true);
-	t.format(-1 * 60 * 10000, '-10 minutes', true);
+	format(60 * 1000, '1 minute', true);
+	format(60 * 1200, '1 minute', true);
+	format(60 * 10000, '10 minutes', true);
+	format(-1 * 60 * 1000, '-1 minute', true);
+	format(-1 * 60 * 1200, '-1 minute', true);
+	format(-1 * 60 * 10000, '-10 minutes', true);
 
 	// hours
-	t.format(60 * 60 * 1000, '1 hour', true);
-	t.format(60 * 60 * 1200, '1 hour', true);
-	t.format(60 * 60 * 10000, '10 hours', true);
-	t.format(-1 * 60 * 60 * 1000, '-1 hour', true);
-	t.format(-1 * 60 * 60 * 1200, '-1 hour', true);
-	t.format(-1 * 60 * 60 * 10000, '-10 hours', true);
+	format(60 * 60 * 1000, '1 hour', true);
+	format(60 * 60 * 1200, '1 hour', true);
+	format(60 * 60 * 10000, '10 hours', true);
+	format(-1 * 60 * 60 * 1000, '-1 hour', true);
+	format(-1 * 60 * 60 * 1200, '-1 hour', true);
+	format(-1 * 60 * 60 * 10000, '-10 hours', true);
 
 	// days
-	t.format(24 * 60 * 60 * 1000, '1 day', true);
-	t.format(24 * 60 * 60 * 1200, '1 day', true);
-	t.format(24 * 60 * 60 * 10000, '10 days', true);
-	t.format(-1 * 24 * 60 * 60 * 1000, '-1 day', true);
-	t.format(-1 * 24 * 60 * 60 * 1200, '-1 day', true);
-	t.format(-1 * 24 * 60 * 60 * 10000, '-10 days', true);
+	format(24 * 60 * 60 * 1000, '1 day', true);
+	format(24 * 60 * 60 * 1200, '1 day', true);
+	format(24 * 60 * 60 * 10000, '10 days', true);
+	format(-1 * 24 * 60 * 60 * 1000, '-1 day', true);
+	format(-1 * 24 * 60 * 60 * 1200, '-1 day', true);
+	format(-1 * 24 * 60 * 60 * 10000, '-10 days', true);
 
   // rounding
-	t.format(234234234, '3 days', true);
-	t.format(-234234234, '-3 days', true);
-
-	t.end();
+	format(234234234, '3 days', true);
+	format(-234234234, '-3 days', true);
 });
+
+
+test.run();
